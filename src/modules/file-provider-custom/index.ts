f5bd3372dceac66dd16739962dc382dab8d4b3cf
AX235651251 <bboy10121988@gmail.com>
2025-11-02 02:44:50 +0800

    Remove unused custom file provider - using Nginx rewrite solution instead

--- src/modules/file-provider-custom/index.ts
+++
@@ -1,6 +0,0 @@
-import { ModuleProvider, Modules } from "@medusajs/framework/utils"
-import CustomFileProviderService from "./service.js"
-
-export default ModuleProvider(Modules.FILE, {
-  services: [CustomFileProviderService],
-})
