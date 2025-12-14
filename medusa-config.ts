3af5fd71a3b762022b6b689c490dce6bf7fa1e2b
bboy10121988 <bboy10121988@gmail.com>
2025-12-14 16:07:12 +0800

    revert: remove vite hmr clientPort config

--- medusa-config.ts
+++ medusa-config.ts
@@ -26,9 +26,6 @@ module.exports = defineConfig({
         server: {
           ...config.server,
           allowedHosts: ['admin.timsfantasyworld.com', '.timsfantasyworld.com', 'localhost'],
-          hmr: {
-            clientPort: 443,
-          },
         },
       }
     },
